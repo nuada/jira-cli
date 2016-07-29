e89a69483675ff0c71119093b3bad7f52d554ce9
Piotr Radkowski <piotr.radkowski@ardigen.com>
2016-07-29 16:13:37 +0200

    Use new JIRA class and old GreenHopper API

--- jira-cli.py
+++ jira-cli.py
@@ -3,7 +3,7 @@
 import json
 import sys
 
-from jira.client import GreenHopper
+from jira.client import JIRA, GreenHopperResource
 
 
 def read(file_path):
@@ -13,11 +13,19 @@ def read(file_path):
 
 if __name__ == '__main__':
     config = json.load(open('config.json'))
-    jira = GreenHopper(options={'server': config['server']},
-                       oauth=dict(access_token=config['access_token'],
-                                  access_token_secret=config['access_token_secret'],
-                                  consumer_key=config['consumer_key'],
-                                  key_cert=read('private_key.pem')))
+    jira = JIRA(options=dict(server=config['server'],
+                             agile_rest_path=GreenHopperResource.AGILE_BASE_REST_PATH),
+                oauth=dict(access_token=config['access_token'],
+                           access_token_secret=config['access_token_secret'],
+                           consumer_key=config['consumer_key'],
+                           key_cert=read('private_key.pem')))
+
+    gh = JIRA(options=dict(server=config['server'],
+                           agile_rest_path=GreenHopperResource.GREENHOPPER_REST_PATH),
+              oauth=dict(access_token=config['access_token'],
+                         access_token_secret=config['access_token_secret'],
+                         consumer_key=config['consumer_key'],
+                         key_cert=read('private_key.pem')))
 
     if sys.argv[1] == 'boards':
         for board in jira.boards():
@@ -26,12 +34,12 @@ if __name__ == '__main__':
         board_id = int(sys.argv[2])
         for sprint in jira.sprints(board_id, extended=True):
             print('{} {} {}'.format(sprint.startDate, sprint.endDate, sprint.name))
-            completed = jira.completed_issues(board_id, sprint.id)
+            completed = gh.completed_issues(board_id, sprint.id)
             if completed:
                 print('  Completed:')
                 for issue in completed:
                     print('   * {:5} {:10} {}'.format(issue.key, issue.statusName, issue.summary))
-            incomplete = jira.incompleted_issues(board_id, sprint.id)
+            incomplete = gh.incompleted_issues(board_id, sprint.id)
             if incomplete:
                 print('  Incomplete:')
                 for issue in incomplete:
